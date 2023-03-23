22e41d58936718c3e74517b3efe431c4e34d6cbd
Victor Aurelio <victoraurelio_198@hotmail.com>
2023-03-23 08:09:56 -0300

    Deleting unecessary files

--- frontend/src/products/AddProduct.jsx
+++ frontend/src/products/AddProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './AddProduct.css';
 
 const AddProduct = () => {
     const navigate = useNavigate();
